3a54287d72014299221d7a12e7e42d3237e8fcc9
MRDanesh <Mohammadrezad126@gmail.com>
2021-07-17 18:16:55 -0400

    add main page

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
 import Main from './screens/Main';
-import About from './screens/About/About';
-import Projects from './screens/Projects/Projects'
 import Header from './Header';
 
 const App = () => {
@@ -13,8 +11,6 @@ const App = () => {
                 <Header/>
                 <>
                     <Route path = '/portfolio/' exact component={Main} />
-                    <Route path = '/portfolio/projects' exact component={Projects}/>
-                    <Route path = '/portfolio/about' component={About} />
                 </>
             </BrowserRouter>
         </>
